dd5d3ff042a393e0351e95b9d6cefb29660308a5
josepcastellsV <joca458@vidalibarraquer.net>
2021-11-03 10:35:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projecte_v1/Register.java
@@ -0,0 +1,14 @@
+package com.example.projecte_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
